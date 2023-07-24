44836bcdae8d72d0282e655cca20a8d3c82fcac9
Jisuz Krist <jisuzkrist@gmail.com>
2023-07-24 18:37:44 +0300

    Test 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // Libraries
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 // (end of) Libraries
 
 // Components
@@ -14,9 +14,11 @@ import "./scss/app.scss";
 export const App: React.FC = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<MainLayout />}></Route>
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}></Route>
+        </Routes>
+      </HashRouter>
     </div>
   );
 };
